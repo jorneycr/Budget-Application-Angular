f9d303af2d9522a8740e9a3200415c204bf6e130
Jorney Lopez <jorneycr@hotmail.com>
2025-04-04 14:07:48 -0600

    changes in model and service

--- src/app/egreso/egreso.service.ts
+++ src/app/egreso/egreso.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Egreso } from './egreso.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EgresoService {
 
-  constructor() { }
+  egreso: Egreso[] = [
+    new Egreso('Renta Depto', 900),
+    new Egreso('Ropa', 200)
+  ];
+
 }
--- src/app/ingreso/ingreso.service.ts
+++ src/app/ingreso/ingreso.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Ingreso } from './ingreso.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresoService {
 
-  constructor() { }
+  ingreso: Ingreso [] = [
+    new Ingreso('Salario', 400),
+    new Ingreso('Venta Coche', 500)
+  ];
+  
 }
